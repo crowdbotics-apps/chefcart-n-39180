33f244ad1e5c694a00de858be8628c0bfc3f6955
Crowdbotics <team@crowdbotics.com>
2023-03-01 18:26:48 +0000

    Initial commit.

---
+++ backend/home/migrations/0001_load_initial_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def create_site(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    custom_domain = "chefcart-n-39180.botics.co"
+
+    site_params = {
+        "name": "ChefCart_N",
+    }
+    if custom_domain:
+        site_params["domain"] = custom_domain
+
+    Site.objects.update_or_create(defaults=site_params, id=1)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_site),
+    ]
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chefcart-n-39180",
+  url: "https://chefcart-n-39180.botics.co",
+  api: "https://chefcart-n-39180.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
